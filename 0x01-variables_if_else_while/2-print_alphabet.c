6f495dee805b0f4c6925c09bae33fedffc210201
Gbenga Joshua Afolabi <joshuaafolabi80@gmail.com>
2022-09-09 21:58:25 -0700

    I sometimes suffer from insomnia.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,20 @@
-#include <stdio.h>
+   #include<stdio.h>
 
-/**
- * main - lowercase alphabets
- *
- * Return: Always (Success)
- */
-int main(void)
-{
+   /**
+   * main - lowercase alphabets
+   *
+   * Return: Always (Success)
+   */
+   int main(void)
+
+   {
 	 char c;
 
 	 for (c = 'a'; c <= 'z'; c++)
-		   putchar(c);
+	 putchar(c);
 
 	 putchar('\n')
 	 return (0);
 
-}
+   }
 
